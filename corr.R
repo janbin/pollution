5f4bbf9ff7559190299732d1d292afdb0fbb45d8
janbin <janbinkimo@gmail.com>
2018-04-22 23:03:27 +0800

    work of quest 3, but still some problems

--- corr.R
+++ corr.R
@@ -1,20 +1,23 @@
 corr <- function(directory1, threshold = 0){
     correlation <- 0
-    for( i in 1:332){
-        if (i <10)
-            csvlist1 <- read.csv(paste(directory1, '/00',toString(i),'.csv',sep = ''))
-        if (i >= 10 & i < 100)
-            csvlist1 <- read.csv(paste(directory1,'/0',toString(i),'.csv', sep = ''))
-        if(i >= 100){
-            csvlist1 <- read.csv(paste(directory1,'/',toString(i),'.csv', sep = ''))
+    thresholdt <- complete(directory1)
+    print("complete function complete")
+    for( z in 1:332){
+        print(paste(toString(z), "st start", sep = ""))
+        if (z <10)
+            csvlist1 <- read.csv(paste(directory1, '/00',toString(z),'.csv',sep = ''))
+        if (z >= 10 & z < 100)
+            csvlist1 <- read.csv(paste(directory1,'/0',toString(z),'.csv', sep = ''))
+        if(z >= 100){
+            csvlist1 <- read.csv(paste(directory1,'/',toString(z),'.csv', sep = ''))
         }
+        print("read file complete")
         
         
-        thresholdt <- complete(directory1)[i, 2]
-        if (thresholdt > threshold){
-            correlation <- rbind(correlation, c(i, cor(csvlist1["nitrate"], csvlist1["sulfate"])))
+        if (thresholdt[z, 2] > threshold){
+            correlation <- rbind(correlation, c(z, cor(csvlist1["nitrate"], csvlist1["sulfate"], use = "complete.obs")))
         }
-        
+        print(paste(toString(z), "st complete", sep = ""))
     }
     correlation
 }
