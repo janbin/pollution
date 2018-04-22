6700d988ab27996fa56c193c107056c829b7f9c2
janbin <janbinkimo@gmail.com>
2018-04-22 17:58:12 +0800

    modified quest 3, can't work

--- corr.R
+++ corr.R
@@ -1,3 +1,20 @@
-corr <- function(directory, threshold = 0){
-  
+corr <- function(directory1, threshold = 0){
+    correlation <- 0
+    for( i in 1:332){
+        if (i <10)
+            csvlist1 <- read.csv(paste(directory1, '/00',toString(i),'.csv',sep = ''))
+        if (i >= 10 & i < 100)
+            csvlist1 <- read.csv(paste(directory1,'/0',toString(i),'.csv', sep = ''))
+        if(i >= 100){
+            csvlist1 <- read.csv(paste(directory1,'/',toString(i),'.csv', sep = ''))
+        }
+        
+        
+        thresholdt <- complete(directory1)[i, 2]
+        if (thresholdt > threshold){
+            correlation <- rbind(correlation, c(i, cor(csvlist1["nitrate"], csvlist1["sulfate"])))
+        }
+        
+    }
+    correlation
 }
